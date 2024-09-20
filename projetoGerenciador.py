2913b47bb5cb90991dabc090628c06d718093dfe
dayvson silva <dayvsonsilva681@gmail.com>
2024-09-20 19:57:32 -0300

    primeiro commit projeto

---
+++ projetoGerenciador.py
@@ -0,0 +1,7 @@
+
+
+
+while True:
+    print("Escolha as opções abaixo:")
+    print("Digite 1 para Adcionar Tarefa \nDigite 2 para listar Tarefa \nDigite 3 para Editar Tarefa \nDigite 4 para Deletar \n:")
+
