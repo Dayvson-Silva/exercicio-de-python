3cba0ef3a0a6af24043e0255b5923e362deb31bc
Ryhan Nalbert <ryhannalbert@gmail.com>
2024-09-20 20:46:14 -0300

    Criação do menu

--- projetoGerenciador.py
+++ projetoGerenciador.py
@@ -1,8 +1,25 @@
 
-
-
 while True:
-    print("Escolha as opções abaixo:")
-    print("Digite 1 para Adcionar Tarefa \nDigite 2 para listar Tarefa \nDigite 3 para Editar Tarefa \nDigite 4 para Deletar ")
-    opcao = int(input(":"))
 
+    option = int(input("\n1 - Adicionar \n2 - Listar \n3 - Editar \n4 - Deletar \n5 - Sair \nDigite uma das opções listadas acima: "))
+
+    match option:
+        case 1:
+            print("Você escolheu adicionar uma tarefa, siga os passos abaixo:")
+            adicionar()
+        
+        case 2:
+            print("Você escolheu listar uma tarefa, siga os passos abaixo:")
+            listar()
+        
+        case 3:
+            print("Você escolheu editar uma tarefa, siga os passos abaixo:")
+            editar()
+        
+        case 4:
+            print("Você escolheu deletar uma tarefa, siga os passos abaixo:")
+            deletar()
+        
+        case 5:
+            print("Você escolheu sair de tudo.")
+            break
