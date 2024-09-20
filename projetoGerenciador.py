c37e8da5a1eb8e9efadd9e80b864dc43dff5b43a
dayvson silva <dayvsonsilva681@gmail.com>
2024-09-20 20:48:16 -0300

    criando funções

--- projetoGerenciador.py
+++ projetoGerenciador.py
@@ -1,8 +1,35 @@
 
 
 
-while True:
-    print("Escolha as opções abaixo:")
-    print("Digite 1 para Adcionar Tarefa \nDigite 2 para listar Tarefa \nDigite 3 para Editar Tarefa \nDigite 4 para Deletar ")
-    opcao = int(input(":"))
+
+
+tarefas = []
+
+def add_tarefa():
+    
+    tarefa ={
+        "nome": input("Digite um nome para a tarefa: "),
+        "Descrição": input("Digite a descrição da tarefa: "),
+        "prioridade": input("Digite a prioridade da tarefa: "),
+        "categoria":  input("Digite a categoria da tarefa: "),
+        "Status": False
+    }
+    tarefas.append(tarefa)
+
+def listar_tarefas():
+    
+
+
+
+    while True:
+        print("digite 1")
+        op = int(input("digite a opção:"))
+        if op == 1:
+            add_tarefa()
+    
+      
+       
+       
+
+
 
