09ad09253501edbc0394612670b8e64b714e2070
dayvson silva <dayvsonsilva681@gmail.com>
2024-09-20 20:00:07 -0300

    eu

--- projetoGerenciador.py
+++ projetoGerenciador.py
@@ -3,5 +3,6 @@
 
 while True:
     print("Escolha as opções abaixo:")
-    print("Digite 1 para Adcionar Tarefa \nDigite 2 para listar Tarefa \nDigite 3 para Editar Tarefa \nDigite 4 para Deletar \n:")
+    print("Digite 1 para Adcionar Tarefa \nDigite 2 para listar Tarefa \nDigite 3 para Editar Tarefa \nDigite 4 para Deletar ")
+    opcao = int(input(":"))
 
